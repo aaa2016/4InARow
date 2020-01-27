2e9b184a621402d5d71e4a660ba9ba679e9442ef
aaa2016 <artur.arikainen@gmail.com>
2020-01-27 21:11:40 +0000

    Playable with automated win checking
    The game will now automatically check if either player gets 4 discs in a row

--- 4inarow.js
+++ 4inarow.js
@@ -1,16 +1,19 @@
+// Four In A Row (2-player game)
 
 // default game board is 7x6
 var boardCols = 7;
 var boardRows = 6; // not including row for input buttons
 var maxCells = boardCols * boardRows;
 
-var winningRun = 4; // need 4 in a row to win
+var winningRun = 4; // need 4 in a row to win - note: auto win checking will remain at 4
 var gameOver = false; // state for when the game ends
 var redsTurn = true; // red goes first
 
 // counters for pieces dropped
 var redPieces = 0;
 var bluePieces = 0;
+var redTerritory = [];
+var blueTerritory = [];
 
 var winner = "";
 
@@ -22,11 +25,14 @@ function newGame() {
 	gameOver = false;
 	document.getElementById("game-board").innerHTML = "";
 	createGameBoard(boardCols, boardRows);
-	document.getElementById("message-area").innerHTML = "New game started.";
+	document.getElementById("message-area").innerHTML = 
+		"New game started - red goes first.";
 	gameOver = false;
 	redsTurn = true;
 	redPieces = 0;
 	bluePieces = 0;
+	redTerritory = [];
+	blueTerritory = [];
 	winner = "";
 	
 };
@@ -52,7 +58,7 @@ function createGameBoard(cols, rows) {
 
     	for (var j = 0; j < cols; j++) {
       		var td = document.createElement('TD');
-      		//td.appendChild(document.createTextNode(i+","+j));
+      		//td.appendChild(document.createTextNode(i+","+j)); // label cell for debug
       		var cellDiv = document.createElement('DIV');
       		var button = document.createElement('BUTTON');
       		
@@ -82,6 +88,7 @@ function createGameBoard(cols, rows) {
 function dropDisc(col) { 
 
 	var discColour = "";
+	var nextPlayer = "";
 	var discDiv = document.createElement('DIV'); // element to draw disc
 	
 	var emptyRow = boardRows-1; //starts with lowest row
@@ -89,16 +96,24 @@ function dropDisc(col) {
 	for (emptyRow; emptyRow>=0; emptyRow--) { 
 	
 		// checks if row is empty, otherwise continues to higher row
-		if (document.getElementById(emptyRow+","+col).innerHTML == "") {
+		if (gameOver==false && document.getElementById(emptyRow+","+col).innerHTML == "") {
 			document.getElementById(emptyRow+","+col).innerHTML = "";
 			
 			// check whose turn it is to set colour
 			if (redsTurn == true) {
 				discColour = "red";
+				//redTerritory.push("["+emptyRow+","+col+"]");
+				redTerritory.push(emptyRow+","+col);
+				redTerritory.sort();
+				nextPlayer = "blue";
 				redPieces++;
 				redsTurn = false;
 			} else {
 				discColour = "blue";
+				//blueTerritory.push("["+emptyRow+","+col+"]");
+				blueTerritory.push(emptyRow+","+col);
+				blueTerritory.sort();
+				nextPlayer = "red";
 				bluePieces++;
 				redsTurn = true;
 			};
@@ -106,9 +121,16 @@ function dropDisc(col) {
 			// set disc colour and add to cell
 			discDiv.setAttribute("class", discColour+"-disc");
 			document.getElementById(emptyRow+","+col).appendChild(discDiv);
-			document.getElementById("message-area").innerHTML = "Disc dropped by "
-				+discColour+" in col "+col+". Total red: "+redPieces+". Total blue: "
-				+bluePieces+".";
+			
+			// for debug:
+			//document.getElementById("message-area").innerHTML = "Disc dropped by "
+			//	+discColour+" in col "+col+" - "+nextPlayer+" goes next. Total red: "
+			//	+redPieces+". Total blue: "+bluePieces+". \n"+
+			//	"Red territory: " +redTerritory+ ". Blue territory: "+blueTerritory+".";
+			
+			document.getElementById("message-area").innerHTML = "Next player: "
+				+nextPlayer;
+				
 			break; // can exit for loop after first empty cell
 		};
 	
@@ -122,7 +144,82 @@ function dropDisc(col) {
 
 function checkWin() {
 
-	// TODO: add win state checking
+	// for debug:
+	//document.getElementById("win-message-area").innerHTML = "Checking win states.";
+	
+	var redDiscCounter = [0,0,0,0];
+	var blueDiscCounter = [0,0,0,0];
+	
+	// cycle through each cell on the board as an anchor point
+	for (i=0; i<boardRows; i++) {
+		for (j=0; j<boardCols; j++) {
+			
+			// 4 win patterns possible: horizontal, vertical, down diagonal, up diagonal
+			// coordinates: [row,col] = [i,j], which is the anchor point
+			// note: currently this will always check 4 discs in a row
+			
+			// horizontal
+			var h = [[i,j],[i,j+1],[i,j+2],[i,j+3]];
+	
+			// vertical
+			var v = [[i,j],[i+1,j],[i+2,j],[i+3,j]];
+	
+			// down diagonal
+			var dd = [[i,j],[i+1,j+1],[i+2,j+2],[i+3,j+3]];
+	
+			// up diagonal
+			var ud = [[i,j],[i+1,j-1],[i+2,j-2],[i+3,j-3]];
+			
+			var allWinStates = [h,v,dd,ud];
+			
+			// reset counters for each anchor point
+			redDiscCounter = [0,0,0,0];
+			blueDiscCounter = [0,0,0,0];
+			
+			// cycle through territories to find matches in territory held
+			for (k=0; k<redTerritory.length; k++) {
+				for (l=0; l<allWinStates.length; l++) {
+					for (m=0; m<allWinStates[l].length; m++) {
+						
+						// for debug:
+						//document.getElementById("win-message-area").innerHTML = 
+						//		"i="+i+",j="+j+",k="+k+",l="+l+" - h="+h+"; redcounter="
+						//		+redDiscCounter+"; bluecounter="+blueDiscCounter;
+						
+						if (redTerritory[k]==allWinStates[l][m]) {
+							redDiscCounter[l]++; // if match found, increment counter
+							if (redDiscCounter[l] >= winningRun) { // winningRun set in header
+								winner = "red";
+								gameOver = true;
+							};
+						};
+					}; //m loop
+				}; // l loop
+			}; // k loop
+			
+			for (k=0; k<blueTerritory.length; k++) {
+				for (l=0; l<allWinStates.length; l++) {
+					for (m=0; m<allWinStates[l].length; m++) {
+					
+						// for debug:
+						//document.getElementById("win-message-area").innerHTML = 
+						//		"i="+i+",j="+j+",k="+k+",l="+l+" - h="+h+"; redcounter="
+						//		+redDiscCounter+"; bluecounter="+blueDiscCounter;
+						
+						if (blueTerritory[k]==allWinStates[l][m]) {
+							blueDiscCounter[l]++; // if match found, increment counter
+							if (blueDiscCounter[l] >= winningRun) { // winningRun set in header
+								winner = "blue";
+								gameOver = true;
+							};
+						};
+					}; //m loop
+				}; // l loop
+			}; // k loop
+
+		}; // j loop
+	}; // i loop
+	
 
 	// if total pieces dropped reach mak number of cells, game is over
 	if ((redPieces+bluePieces) >= (maxCells)) {
@@ -139,5 +236,3 @@ function checkWin() {
 };
 
 ///////////////////////////////////////
-
-
